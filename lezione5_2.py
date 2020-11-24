ffe988259f9df9e50d3b599efb21d589438c71ab
repl.it user <replituser@example.com>
2020-11-24 17:04:35 +0000

    lezione 5_2

---
+++ lezione5_2.py
@@ -0,0 +1,3 @@
+# Modificate l'oggetto CSVFile della lezione precedente in modo che alzi un'eccezione se il nome del file non è una stringa.
+# Poi, modificate la funzione get_data in modo da leggere solo un intervallo di righe del file, aggiungendo gli argomenti start ed end opzionali, controllandone la correttezza e sanitizzandoli se necessario.
+
